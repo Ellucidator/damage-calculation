7f0b7b54eb645a5ccc12068de0c28b1c14342f6e
Marcelo Leal <andradefj13@hotmail.com>
2023-12-07 19:59:02 -0300

    UpdateCSS
    Projeto feito para treinar o css, basicamente foi feito só estilização e organização da pagina, nenhuma funcionalidade

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
+import './app.css'
 function App() {
 
   return (
-    <div>
-      
+    <div className="app">
+      <div className='container'>
+        <h1>Guerreiro</h1>
+        <img src="../public/Guerreiro.png" alt="" className='img-character' />
+        <div className='atq'>40</div>
+      </div>
+      <div className='container damage'>
+        <h1>Dano de Ataque</h1>
+        <div className='img-damage'></div>
+        <span className='atq damage-valor'>
+        100
+        </span>
+      </div>
+      <div className='container'>
+        <h1>Paladino</h1>
+        <img src="../public/Paladin.png" alt="" className='img-character' />
+        <div className='def'>
+          <span className='def-item shield'>40</span>
+          <span className='def-item life'>40</span>
+        </div>
+      </div>
     </div>
   )
 }
